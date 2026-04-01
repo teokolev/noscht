55e02e9200c2ad0eeb8ab7a68ad5b4afc908f82b
teokolev <teddyrkolev@gmail.com>
2026-04-01 19:17:17 -0400

    test2026

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,6 +1,6 @@
 let mappedData = allProjects.map((a => {
         if (a.isVideo) {
-                return `<div class="parents parent_${a.id} scroll_images" id="parent_${a.id}"><div class="video_box_${a.id}" id="video_box_${a.id}"><video class="" id="video_${a.id}" muted preload="metadata" src="${a.projectMedia}" type="video/mp4"></video></div><div class="text_P${a.id}" id="text_P${a.id}"><div class="P${a.id}_line_1" id="P${a.id}_line_1"></div><div class="P${a.id}_line_2" id="P${a.id}_line_2"></div><div class="P${a.id}_line_3" id="P${a.id}_line_3"></div></div></div>`
+                return `<div class="parents parent_${a.id} scroll_images" id="parent_${a.id}"><div class="video_box_${a.id}" id="video_box_${a.id}"><video class="" id="video_${a.id}" muted autoplay playsinline preload="metadata" src="${a.projectMedia}" type="video/mp4"></video></div><div class="text_P${a.id}" id="text_P${a.id}"><div class="P${a.id}_line_1" id="P${a.id}_line_1"></div><div class="P${a.id}_line_2" id="P${a.id}_line_2"></div><div class="P${a.id}_line_3" id="P${a.id}_line_3"></div></div></div>`
         } else {
                 return `<div class="parents parent_${a.id} scroll_images" id="parent_${a.id}"><div class="image_box_${a.id}" id="image_box_${a.id}"><img class="image_${a.id}" src="${a.projectMedia}" style="z-index: -999;"></div><div class="text_P${a.id}" id="text_P${a.id}"><div class="P${a.id}_line_1" id="P${a.id}_line_1"></div><div class="P${a.id}_line_2" id="P${a.id}_line_2"></div><div class="P${a.id}_line_3" id="P${a.id}_line_3"></div></div></div>`
         }
@@ -40,29 +40,61 @@ for (let k=0;k<allProjects.length;k++) {
                 if (a.isFirst) {
                         if (a.isText) {
                                 if (a.isVideo) {
-                                        return `<div class="parents_m parent_${a.id}_m" id="parent_${a.id}_m"><div class="video_box_${a.id}_m" id="video_box_${a.id}_m"><video class="scroll_modal" id="video_${a.id}_m" muted preload="metadata" src="${a.assetMedia}" type="video/mp4"></video><div class="hero-text" id="hero-text"></div></div><div class="text_m" id="text_m${a.id}"><div class="m${a.id}_line_1" id="m${a.id}_line_1">${a.firstText}</div><div class="m${a.id}_line_2" id="m${a.id}_line_2">${a.secondText}</div></div></div>`
+                                        if (a.isSquare) {
+                                                return `<div class="parents_m square_asset_modal" id="parent_${a.id}_m"><div class="video_box_modal" id="video_box_${a.id}_m"><video class="scroll_modal" id="video_${a.id}_m" muted autoplay playsinline preload="metadata" src="${a.assetMedia}" type="video/mp4"></video><div class="hero-text" id="hero-text"></div></div><div class="text_m" id="text_m${a.id}"><div class="m${a.id}_line_1" id="m${a.id}_line_1">${a.firstText}</div><div class="m${a.id}_line_2" id="m${a.id}_line_2">${a.secondText}</div></div></div>`
+                                        } else {
+                                                return `<div class="parents_m nonsquare_asset_modal" id="parent_${a.id}_m"><div class="video_box_modal" id="video_box_${a.id}_m"><video class="scroll_modal" id="video_${a.id}_m" muted autoplay playsinline preload="metadata" src="${a.assetMedia}" type="video/mp4"></video><div class="hero-text" id="hero-text"></div></div><div class="text_m" id="text_m${a.id}"><div class="m${a.id}_line_1" id="m${a.id}_line_1">${a.firstText}</div><div class="m${a.id}_line_2" id="m${a.id}_line_2">${a.secondText}</div></div></div>`
+                                        }
                                 } else {
-                                        return `<div class="parents_m parent_${a.id}_m" id="parent_${a.id}_m"><div class="image_box_${a.id}_m" id="image_box_${a.id}_m"><img class="scroll_modal image_${a.id}_m" src="${a.assetMedia}" style="z-index: -999;"><div class="hero-text" id="hero-text"></div></div><div class="text_m" id="text_m${a.id}"><div class="m${a.id}_line_1" id="m${a.id}_line_1">${a.firstText}</div><div class="m${a.id}_line_2" id="m${a.id}_line_2">${a.secondText}</div></div></div>`
+                                        if (a.isSquare) {
+                                                return `<div class="parents_m square_asset_modal" id="parent_${a.id}_m"><div class="image_box_modal" id="image_box_${a.id}_m"><img class="scroll_modal" src="${a.assetMedia}" style="z-index: -999;"><div class="hero-text" id="hero-text"></div></div><div class="text_m" id="text_m${a.id}"><div class="m${a.id}_line_1" id="m${a.id}_line_1">${a.firstText}</div><div class="m${a.id}_line_2" id="m${a.id}_line_2">${a.secondText}</div></div></div>`
+                                        } else {
+                                                return `<div class="parents_m nonsquare_asset_modal" id="parent_${a.id}_m"><div class="image_box_modal" id="image_box_${a.id}_m"><img class="scroll_modal" src="${a.assetMedia}" style="z-index: -999;"><div class="hero-text" id="hero-text"></div></div><div class="text_m" id="text_m${a.id}"><div class="m${a.id}_line_1" id="m${a.id}_line_1">${a.firstText}</div><div class="m${a.id}_line_2" id="m${a.id}_line_2">${a.secondText}</div></div></div>`
+                                        }
                                 }
                         } else {
                                 if (a.isVideo) {
-                                        return `<div class="parents_m parent_${a.id}_m" id="parent_${a.id}_m"><div class="video_box_${a.id}_m" id="video_box_${a.id}_m"><video class="scroll_modal" id="video_${a.id}_m" muted preload="metadata" src="${a.assetMedia}" type="video/mp4"></video><div class="hero-text" id="hero-text"></div></div></div>`
+                                        if (a.isSquare) {
+                                                return `<div class="parents_m square_asset_modal" id="parent_${a.id}_m"><div class="video_box_modal" id="video_box_${a.id}_m"><video class="scroll_modal" id="video_${a.id}_m" muted autoplay playsinline preload="metadata" src="${a.assetMedia}" type="video/mp4"></video><div class="hero-text" id="hero-text"></div></div></div>`
+                                        } else {
+                                                return `<div class="parents_m nonsquare_asset_modal" id="parent_${a.id}_m"><div class="video_box_modal" id="video_box_${a.id}_m"><video class="scroll_modal" id="video_${a.id}_m" muted autoplay playsinline preload="metadata" src="${a.assetMedia}" type="video/mp4"></video><div class="hero-text" id="hero-text"></div></div></div>`
+                                        }
                                 } else {
-                                        return `<div class="parents_m parent_${a.id}_m" id="parent_${a.id}_m"><div class="image_box_${a.id}_m" id="image_box_${a.id}_m"><img class="scroll_modal image_${a.id}_m" src="${a.assetMedia}" style="z-index: -999;"><div class="hero-text" id="hero-text"></div></div></div>`
+                                        if (a.isSquare) {
+                                                return `<div class="parents_m square_asset_modal" id="parent_${a.id}_m"><div class="image_box_modal" id="image_box_${a.id}_m"><img class="scroll_modal" src="${a.assetMedia}" style="z-index: -999;"><div class="hero-text" id="hero-text"></div></div></div>`
+                                        } else {
+                                                return `<div class="parents_m nonsquare_asset_modal" id="parent_${a.id}_m"><div class="image_box_modal" id="image_box_${a.id}_m"><img class="scroll_modal" src="${a.assetMedia}" style="z-index: -999;"><div class="hero-text" id="hero-text"></div></div></div>`
+                                        }
                                 }
                         }
                 } else {
                         if (a.isText) {
                                 if (a.isVideo) {
-                                        return `<div class="parents_m parent_${a.id}_m" id="parent_${a.id}_m"><div class="video_box_${a.id}_m" id="video_box_${a.id}_m"><video class="scroll_modal" id="video_${a.id}_m" muted preload="metadata" src="${a.assetMedia}" type="video/mp4"></video></div><div class="text_m" id="text_m${a.id}"><div class="m${a.id}_line_1" id="m${a.id}_line_1">${a.firstText}</div><div class="m${a.id}_line_2" id="m${a.id}_line_2">${a.secondText}</div></div></div>`
+                                        if (a.isSquare) {
+                                                return `<div class="parents_m square_asset_modal" id="parent_${a.id}_m"><div class="video_box_modal" id="video_box_${a.id}_m"><video class="scroll_modal" id="video_${a.id}_m" muted autoplay playsinline preload="metadata" src="${a.assetMedia}" type="video/mp4"></video></div><div class="text_m" id="text_m${a.id}"><div class="m${a.id}_line_1" id="m${a.id}_line_1">${a.firstText}</div><div class="m${a.id}_line_2" id="m${a.id}_line_2">${a.secondText}</div></div></div>`
+                                        } else {
+                                                return `<div class="parents_m nonsquare_asset_modal" id="parent_${a.id}_m"><div class="video_box_modal" id="video_box_${a.id}_m"><video class="scroll_modal" id="video_${a.id}_m" muted autoplay playsinline preload="metadata" src="${a.assetMedia}" type="video/mp4"></video></div><div class="text_m" id="text_m${a.id}"><div class="m${a.id}_line_1" id="m${a.id}_line_1">${a.firstText}</div><div class="m${a.id}_line_2" id="m${a.id}_line_2">${a.secondText}</div></div></div>`
+                                        }
                                 } else {
-                                        return `<div class="parents_m parent_${a.id}_m" id="parent_${a.id}_m"><div class="image_box_${a.id}_m" id="image_box_${a.id}_m"><img class="scroll_modal image_${a.id}_m" src="${a.assetMedia}" style="z-index: -999;"></div><div class="text_m" id="text_m${a.id}"><div class="m${a.id}_line_1" id="m${a.id}_line_1">${a.firstText}</div><div class="m${a.id}_line_2" id="m${a.id}_line_2">${a.secondText}</div></div></div>`
+                                        if (a.isSquare) {
+                                                return `<div class="parents_m square_asset_modal" id="parent_${a.id}_m"><div class="image_box_modal" id="image_box_${a.id}_m"><img class="scroll_modal" src="${a.assetMedia}" style="z-index: -999;"></div><div class="text_m" id="text_m${a.id}"><div class="m${a.id}_line_1" id="m${a.id}_line_1">${a.firstText}</div><div class="m${a.id}_line_2" id="m${a.id}_line_2">${a.secondText}</div></div></div>`
+                                        } else {
+                                                return `<div class="parents_m nonsquare_asset_modal" id="parent_${a.id}_m"><div class="image_box_modal" id="image_box_${a.id}_m"><img class="scroll_modal" src="${a.assetMedia}" style="z-index: -999;"></div><div class="text_m" id="text_m${a.id}"><div class="m${a.id}_line_1" id="m${a.id}_line_1">${a.firstText}</div><div class="m${a.id}_line_2" id="m${a.id}_line_2">${a.secondText}</div></div></div>`
+                                        }
                                 }
                         } else {
                                 if (a.isVideo) {
-                                        return `<div class="parents_m parent_${a.id}_m" id="parent_${a.id}_m"><div class="video_box_${a.id}_m" id="video_box_${a.id}_m"><video class="scroll_modal" id="video_${a.id}_m" muted preload="metadata" src="${a.assetMedia}" type="video/mp4"></video></div></div>`
+                                        if (a.isSquare) {
+                                                return `<div class="parents_m square_asset_modal" id="parent_${a.id}_m"><div class="video_box_modal" id="video_box_${a.id}_m"><video class="scroll_modal" id="video_${a.id}_m" muted autoplay playsinline preload="metadata" src="${a.assetMedia}" type="video/mp4"></video></div></div>`
+                                        } else {
+                                                return `<div class="parents_m nonsquare_asset_modal" id="parent_${a.id}_m"><div class="video_box_modal" id="video_box_${a.id}_m"><video class="scroll_modal" id="video_${a.id}_m" muted autoplay playsinline preload="metadata" src="${a.assetMedia}" type="video/mp4"></video></div></div>`
+                                        }
                                 } else {
-                                        return `<div class="parents_m parent_${a.id}_m" id="parent_${a.id}_m"><div class="image_box_${a.id}_m" id="image_box_${a.id}_m"><img class="scroll_modal image_${a.id}_m" src="${a.assetMedia}" style="z-index: -999;"></div></div>`
+                                        if (a.isSquare) {
+                                                return `<div class="parents_m square_asset_modal" id="parent_${a.id}_m"><div class="image_box_modal" id="image_box_${a.id}_m"><img class="scroll_modal" src="${a.assetMedia}" style="z-index: -999;"></div></div>`
+                                        } else {
+                                                return `<div class="parents_m nonsquare_asset_modal" id="parent_${a.id}_m"><div class="image_box_modal" id="image_box_${a.id}_m"><img class="scroll_modal" src="${a.assetMedia}" style="z-index: -999;"></div></div>`
+                                        }
                                 }
                         }
                 }
